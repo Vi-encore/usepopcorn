e759c84bb29e2e552e5e8553f647b1609361dde8
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-09-29 22:52:40 +0300

    useEffect + useRef for search

--- src/App.js
+++ src/App.js
@@ -158,8 +158,18 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(() => {
-    inputEl.current.focus();
-  }, []);
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
 
   return (
     <input
