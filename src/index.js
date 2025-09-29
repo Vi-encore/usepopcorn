00f51dc9d79f9086588f2d133ea98829647d25fb
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-09-29 22:44:29 +0300

    useRef() added

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App-v1";
-// import StarRating from "./StarRating";
-
-// function Test() {
-//   const [movieRating, setMovieRating] = useState(0);
-
-//   return (
-//     <div>
-//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-//       <p>This movie was rated {movieRating} stars</p>
-//     </div>
-//   );
-// }
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}
-      defaultRating={3}
-    /> */}
-    {/* <Test /> */}
   </React.StrictMode>
 );
